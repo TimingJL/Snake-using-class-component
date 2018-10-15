ec5026d13df82c54161b509e8ecc36f330b6e25c
Taiming Chen <eefozeo@gmail.com>
2018-10-15 12:11:00 +0800

    Updates

---
+++ precache-manifest.e11837120efcfbb18380473787fce899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Snake/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "85ec3cfbec47b5c9c920",
+    "url": "/Snake/static/js/runtime~main.85ec3cfb.js"
+  },
+  {
+    "revision": "67e84b427622c3884a90",
+    "url": "/Snake/static/js/main.67e84b42.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Snake/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "67e84b427622c3884a90",
+    "url": "/Snake/static/css/main.705a771e.chunk.css"
+  },
+  {
+    "revision": "f28327ae1431760c76a97da68c55774d",
+    "url": "/Snake/index.html"
+  }
+];
