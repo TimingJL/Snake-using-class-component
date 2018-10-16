cec5478e56fd8c7d36b5484f22ca3a5fee2ca200
Taiming Chen <eefozeo@gmail.com>
2018-10-16 16:14:22 +0800

    Updates

---
+++ precache-manifest.d55d370de34064f9b4dd88f55f277cf9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85ec3cfbec47b5c9c920",
+    "url": "/Snake/static/js/runtime~main.85ec3cfb.js"
+  },
+  {
+    "revision": "677dc948e026e608d1cb",
+    "url": "/Snake/static/js/main.677dc948.chunk.js"
+  },
+  {
+    "revision": "4b187b566614e8492ed0",
+    "url": "/Snake/static/js/1.4b187b56.chunk.js"
+  },
+  {
+    "revision": "677dc948e026e608d1cb",
+    "url": "/Snake/static/css/main.070e336d.chunk.css"
+  },
+  {
+    "revision": "9d8598e1a2ba50bd286d04c1b337bdf6",
+    "url": "/Snake/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/Snake/precache-manifest.a61601b7fe3633f01b93ec618621d5f5.js"
+  "/Snake/precache-manifest.d55d370de34064f9b4dd88f55f277cf9.js"
 );
 
 workbox.clientsClaim();
